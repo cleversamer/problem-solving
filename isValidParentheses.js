e29415a891bed2e622c03ecc174906c33f61bae4
cleversamerr <thedev.samer@gmail.com>
2024-02-18 10:45:40 +0200

    solved the valid parentheses problem

---
+++ isValidParentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = (s) => {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const top = stack[stack.length - 1];
+
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      stack.push(s[i]);
+    } else if (s[i] === ")" && top === "(" && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === "]" && top === "[" && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === "}" && top === "{" && stack.length !== 0) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
